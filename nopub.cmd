9639f08872761d24e83c00cb87f3e29bd95230ad
Doug Theobald <dtheobald@worksoft.com>
2019-04-24 09:26:33 -0500

    Plugin release 10.0.1

--- nopub.cmd
+++
@@ -1,9 +0,0 @@
-git rm Worksoft+Execution+Manager+Plugin.doc
-git rm git_tips.md
-git rm gradle-wsbuilduser.properties
-git rm jenkinsPipeline.gdsl
-git rm run.bat
-git rm versioner.ps1
-
-git rm nopub.cmd
-
